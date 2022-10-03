8a7d8ca01c4c515a32de8dd3c50e82d3cab0bc1f
Lexus Banton <lexusbanton.kr@gmail.com>
2022-10-03 14:49:40 -0700

    uncommented redirect

--- pet-editor/pet-editor.js
+++ pet-editor/pet-editor.js
@@ -51,7 +51,7 @@ petForm.addEventListener('submit', async (e) => {
     if (error) {
         displayError();
     } else {
-        //location.assign('/');
+        location.assign('/');
     }
 });
 
